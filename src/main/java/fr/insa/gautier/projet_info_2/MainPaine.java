2ee351222e41b3e53b47d45e2d2e09a4e8b2148b
emarx02 <emarx02@SEC-C210-F12.csip.in>
2024-05-10 15:08:54 +0200

    debut de la lecture de rev

--- src/main/java/fr/insa/gautier/projet_info_2/MainPaine.java
+++ src/main/java/fr/insa/gautier/projet_info_2/MainPaine.java
@@ -4,6 +4,13 @@
  */
 package fr.insa.gautier.projet_info_2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.canvas.Canvas;
@@ -58,6 +65,8 @@ public class MainPaine extends BorderPane {
     
     public void bcNouvelEtage(){
         System.out.println("fpsdojfpjospdojfpsojdfs");
+        ArrayList<Revetement> Revetements = new ArrayList();
+        LectureRevetements(Revetements);
     }
     public void bcNouvellePiece(){
         System.out.println("suzette");
@@ -69,4 +78,70 @@ public class MainPaine extends BorderPane {
         System.out.println("des crêpes");
     }
     
+    public void LectureRevetements(ArrayList<Revetement> Revetements) {
+	try {
+            // Création d'un fileReader pour lire le fichier
+            FileReader fileReader = new FileReader("CatalogueRevetements.txt");
+            
+            // Création d'un bufferedReader qui utilise le fileReader
+            BufferedReader reader = new BufferedReader(fileReader);
+			
+            // une fonction à essayer pouvant générer une erreur
+            String line = reader.readLine();
+            
+            int separateurs[] = new int[5];
+            int compt = 0;
+            int tempId = 0;
+            double tempPrix = 0;
+            boolean tempPourSol, tempPourMur,tempPourPlafond;
+            String tempNom;
+            char tempChar[] = new char[20];
+            int faireUnNouveau = 0;
+            int indice = 0;
+                        
+            while (line != null) {
+                // affichage de la ligne
+                System.out.println(line);
+                
+                for (int i=0; i<line.length(); i++) {
+                    if (";".equals(line.charAt(i))) { //on crée un tabeau avec les emplacements des ";"
+                        separateurs[compt]=i;
+                        compt++;
+                    }
+                }
+                line.getChars(separateurs[0], separateurs[1], tempChar, 0);
+                tempNom = Arrays.toString(tempChar);
+                tempPourMur = ("1".equals(line.charAt(separateurs[1]+1)));
+                tempPourSol = ("1".equals(line.charAt(separateurs[2]+1)));
+                tempPourPlafond = ("1".equals(line.charAt(separateurs[3]+1)));
+                
+                for (int i=0; i<line.length(); i++){
+                    if (!(";".equals(line.charAt(i)))) {
+                        if (i < separateurs[0]) {
+                            tempId += line.charAt(i) * Math.pow(10, separateurs[0]-i);
+                        }
+                        if (i > separateurs[4]) {
+                            tempPrix +=line.charAt(i) * Math.pow(10, separateurs[4]-(i+2));
+                        }
+                    }
+                }
+                for (int i = 0; i < indice; i++){
+                    if (!(Revetements.get(i).getIdRev()==tempId)) {
+                        faireUnNouveau = 1;
+                    } else {
+                        faireUnNouveau = 0;
+                    }
+                }
+                if (faireUnNouveau ==1) {
+                    Revetements.add(new Revetement(tempId, tempNom, tempPourPlafond, tempPourSol, tempPourMur, tempPrix));
+                }
+                // lecture de la prochaine ligne
+                line = reader.readLine();
+                indice++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
