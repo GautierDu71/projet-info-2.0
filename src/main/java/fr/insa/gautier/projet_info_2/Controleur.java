92309b7d09c41b3c8e663b038b2ff02e1b4d8e1e
GautierDu71 <gautier.serouart@insa-strasbourg.fr>
2024-05-13 16:56:40 +0200

    controleur

---
+++ src/main/java/fr/insa/gautier/projet_info_2/Controleur.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.gautier.projet_info_2;
+
+/**
+ *
+ * @author gserouart01
+ */
+public class Controleur {
+    
+}
